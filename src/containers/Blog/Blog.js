26680937c3d743d25cb23bf59ecd056910d82af2
Kyle Desktop <kylespresser@gmail.com>
2018-04-06 21:10:16 -0400

    lecture 203"

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,12 +5,18 @@ import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
 // import FullPost from './FullPost/FullPost';
+
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost');
+});
+
 
 class Blog extends Component {
   state = {
-    auth: false
+    auth: true
   }
 
   render () {
@@ -40,7 +46,7 @@ class Blog extends Component {
           {/* <Route path="/" exact render={() => <h1>Home</h1> } />
           <Route path="/" render={() => <h1>Home2</h1> } /> */}
           <Switch>
-            {this.state.auth ? <Route path="/new-post" exact component={NewPost} /> : null}
+            {this.state.auth ? <Route path="/new-post" exact component={AsyncNewPost} /> : null}
             <Route path="/posts" component={Posts} />
             <Route render={() => <h1>Not Found</h1>} />
             {/* <Redirect from="/" to="/posts" /> */}
